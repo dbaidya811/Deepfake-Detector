b82e2f5d6693b6392c5cea99f3982d1cce0388ce
Deep Baidya <124801473+dbaidya811@users.noreply.github.com>
2025-08-18 22:28:10 +0530

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = "https://deepfake-detector-z4no.onrender.com";
 
 const fileInput = document.getElementById("fileInput");
 const preview = document.getElementById("preview");
